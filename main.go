2cbea5370d7d6f6c8404be718314a72c3316ab84
jakubmajer@live.com <jakubmajer@live.com>
2024-05-24 15:06:53 +0200

    zerolog logging

--- main.go
+++ main.go
@@ -2,33 +2,39 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
+	"os"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
 
 func main() {
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+
 	websites := []string{
 		"https://google.com",
 		"https://facebook.com",
 		"https://stackoverflow.com",
 		"https://golang.com",
-		"https://amazon.com",
 		"https://some-random-website-that-does-not-exists.eu",
+		"https://amazon.com",
 	}
 
 	for _, website := range websites {
 		fmt.Printf("Checking: %v\n", website)
 		resp, err := http.Get(website)
 		if err != nil {
-			log.Printf("Error with GET request to %v: %v", website, err)
+			log.Error().Err(err).Msgf("Error with GET request to %v", website)
 			continue
 		}
 		defer resp.Body.Close()
 
 		if resp.StatusCode != http.StatusOK {
-			log.Printf("Unexpected status code for %v: %v\n", website, resp.StatusCode)
+			log.Error().Int("status_code", resp.StatusCode).Msgf("Unexpected status code for %v", website)
 		} else {
-			log.Println("All is good")
+			log.Info().Msgf("All is good with %v", website)
 		}
 
 	}
